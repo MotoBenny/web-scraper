0a4e925a3a68f3971b9339fd18c3ef26022e08d5
Benjamin Carter <benj.james.carter@gmail.com>
2022-05-10 13:46:38 -0700

    began initial strucutre of citation count scraper

--- web_scraper/web_scraper.py
+++ web_scraper/web_scraper.py
@@ -1,4 +1,18 @@
 import requests
+from bs4 import BeautifulSoup
 
-URL = "https://testing-www.codefellows.org/courses/cpde-400"
+URL = "https://en.wikipedia.org/wiki/Alba_(rabbit)"
 page = requests.get(URL)
+
+soup = BeautifulSoup(page.content, "html.parser")
+
+results = soup.find(class_="mw-body-content mw-content-ltr")
+# print(results)
+p_tags = results.find_all('p')
+print(p_tags)
+
+
+def get_citations_needed_count(url):
+
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, "html.parser")
